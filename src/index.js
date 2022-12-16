13e2e9e2e73ab77e3bbe7fef98be19893863a621
Munish Lagoo <munishlagoo@gmail.com>
2022-12-16 15:11:42 -0500

    add1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {ReactDom} from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
 const container = document.getElementById('root');
-const root = ReactDom.createRoot(container);
+const root = createRoot(container);
 
   root.render(
     <React.StrictMode>
